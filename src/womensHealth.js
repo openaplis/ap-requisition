f8698d6dcd973acf097772ab9a3404d9573531a3
Mark Schuetze <markjschuetze@gmail.com>
2017-02-17 14:47:45 -0700

    work on the end product

--- src/womensHealth.js
+++ src/womensHealth.js
@@ -2,5 +2,10 @@ var Handlebars = require('handlebars')
 var templates = require('./template/template.js')
 
 var compileTemplate = Handlebars.templates['topData']
-var result = compileTemplate(topArea.data)
+var compileTemplate = Handlebars.templates['patientInfo']
+var compileTemplate = Handlebars.templates['billingInfo']
+var compileTemplate = Handlebars.templates['womensHealth']
+var compileTemplate = Handlebars.templates['tissueBiopsy']
+
+var result = compileTemplate(topArea.data, patientInfo.data, billingInfo.data, womensHealth.data, tissueBiopsy.data)
 e.innerHTML = result
