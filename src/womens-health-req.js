1ff234886a1e6efdebc614cd7eb8ccb7ab178b91
SidHarder <softwarenavigator@gmail.com>
2017-02-22 09:03:05 -0700

    adding html file folder

--- src/womens-health-req.js
+++ src/womens-health-req.js
@@ -1,15 +1,19 @@
+var fs = require('fs')
 var Handlebars = require('handlebars/runtime');
+
 var templates = require('./precompile-hbs.js');
 
 Handlebars.registerPartial('patient-info', Handlebars.templates['partials/includes/patient-info'])
 Handlebars.registerPartial('billing-info', Handlebars.templates['partials/includes/billing-info'])
 Handlebars.registerPartial('requisition-header', Handlebars.templates['partials/includes/requisition-header'])
 Handlebars.registerPartial('tissue-biopsy', Handlebars.templates['partials/includes/tissue-biopsy'])
-var template = Handlebars.templates['pages/WomensHeatlhReq']
+var template = Handlebars.templates['pages/WomensHealthReq']
+
+var fileName = 'src/requisition/womens-health-req.html'
 
 var d = {
   title: 'hello world'
 }
 
-var result = template(d)
-console.log(result)
+var html = template(d)
+fs.writeFileSync(fileName, html)
