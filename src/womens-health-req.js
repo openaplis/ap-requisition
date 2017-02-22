aec5d20d7fa360451b53753311131f2b33f9e342
SidHarder <softwarenavigator@gmail.com>
2017-02-22 13:52:43 -0700

    adding base functionality

--- src/womens-health-req.js
+++ src/womens-health-req.js
@@ -3,10 +3,14 @@ var Handlebars = require('handlebars/runtime');
 
 var templates = require('./precompile-hbs.js');
 
-Handlebars.registerPartial('patient-info', Handlebars.templates['partials/includes/patient-info'])
-Handlebars.registerPartial('billing-info', Handlebars.templates['partials/includes/billing-info'])
-Handlebars.registerPartial('requisition-header', Handlebars.templates['partials/includes/requisition-header'])
-Handlebars.registerPartial('tissue-biopsy', Handlebars.templates['partials/includes/tissue-biopsy'])
+Handlebars.registerPartial('pages/WomensHealthReq', Handlebars.templates['pages/WomensHealthReq'])
+Handlebars.registerPartial('partials/layouts/base', Handlebars.templates['partials/layouts/base'])
+
+Handlebars.registerPartial('partials/includes/patient-info', Handlebars.templates['partials/includes/patient-info'])
+Handlebars.registerPartial('partials/includes/billing-info', Handlebars.templates['partials/includes/billing-info'])
+Handlebars.registerPartial('partials/includes/requisition-header', Handlebars.templates['partials/includes/requisition-header'])
+Handlebars.registerPartial('partials/includes/tissue-biopsy', Handlebars.templates['partials/includes/tissue-biopsy'])
+
 var template = Handlebars.templates['pages/WomensHealthReq']
 
 var fileName = 'src/requisition/womens-health-req.html'
