01938ba1ed900978c57d11f14c6a289a25276e4d
Mark Schuetze <markjschuetze@gmail.com>
2017-02-22 15:06:02 -0700

    putting it all together

--- src/womens-health-req.js
+++ src/womens-health-req.js
@@ -5,11 +5,11 @@ var templates = require('./precompile-hbs.js');
 
 Handlebars.registerPartial('pages/WomensHealthReq', Handlebars.templates['pages/WomensHealthReq'])
 Handlebars.registerPartial('partials/layouts/base', Handlebars.templates['partials/layouts/base'])
-
 Handlebars.registerPartial('partials/includes/patient-info', Handlebars.templates['partials/includes/patient-info'])
 Handlebars.registerPartial('partials/includes/billing-info', Handlebars.templates['partials/includes/billing-info'])
 Handlebars.registerPartial('partials/includes/requisition-header', Handlebars.templates['partials/includes/requisition-header'])
 Handlebars.registerPartial('partials/includes/tissue-biopsy', Handlebars.templates['partials/includes/tissue-biopsy'])
+Handlebars.registerPartial('partials/includes/womens-health', Handlebars.templates['partials/includes/womens-health'])
 
 var template = Handlebars.templates['pages/WomensHealthReq']
 
