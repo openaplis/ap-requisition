650c3fe2bc198561bc8eb08d8b2e6857e5dfbc09
SidHarder <softwarenavigator@gmail.com>
2017-02-23 10:22:34 -0700

    making classes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-var fs = require("fs")
-var Handlebars = require('Handlebars')
+'use strict'
 
-var source = fs.readFileSync('template/template.js').toString()
-var data = JSON.parse(fs.readFileSync('data/data.json'))
 
-var template = Handlebars.compile(source);
-var result = template(data)
+module.exports = class Requisition {
+  constructor () {
 
-fs.writeFileSync("result.html", result)
-//console.log(result)
+  }
+
+  build(cb) {
+
+  }
+}
--- src/womens-health-req.js
+++ src/womens-health-req.js
@@ -2,21 +2,24 @@ var fs = require('fs')
 var Handlebars = require('handlebars/runtime');
 var templates = require('./hbs-precompile.js');
 
-Handlebars.registerPartial('pages/womens-health-req', Handlebars.templates['pages/womens-health-req'])
-Handlebars.registerPartial('layouts/base', Handlebars.templates['layouts/base'])
-Handlebars.registerPartial('partials/patient-info', Handlebars.templates['partials/patient-info'])
-Handlebars.registerPartial('partials/billing-info', Handlebars.templates['partials/billing-info'])
-Handlebars.registerPartial('partials/requisition-header', Handlebars.templates['partials/requisition-header'])
-Handlebars.registerPartial('partials/tissue-biopsy', Handlebars.templates['partials/tissue-biopsy'])
-Handlebars.registerPartial('partials/womens-health', Handlebars.templates['partials/womens-health'])
+module.exports = class WomensHealthRea {
+  constructor () {
+    Handlebars.registerPartial('pages/womens-health-req', Handlebars.templates['pages/womens-health-req'])
+    Handlebars.registerPartial('layouts/base', Handlebars.templates['layouts/base'])
+    Handlebars.registerPartial('partials/patient-info', Handlebars.templates['partials/patient-info'])
+    Handlebars.registerPartial('partials/billing-info', Handlebars.templates['partials/billing-info'])
+    Handlebars.registerPartial('partials/requisition-header', Handlebars.templates['partials/requisition-header'])
+    Handlebars.registerPartial('partials/tissue-biopsy', Handlebars.templates['partials/tissue-biopsy'])
+    Handlebars.registerPartial('partials/womens-health', Handlebars.templates['partials/womens-health'])
 
-var template = Handlebars.templates['pages/WomensHealthReq']
+    this.template = Handlebars.templates['pages/WomensHealthReq']
+  }
 
-var fileName = 'src/requisition/womens-health-req.html'
+  build(cb) {
+    var d = {
+      title: 'hello world'
+    }
 
-var d = {
-  title: 'hello world'
+    cb(this.template(d))    
+  }
 }
-
-var html = template(d)
-fs.writeFileSync(fileName, html)
