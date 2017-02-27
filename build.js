e707058ea8c34906a39b18d82828c1d58515806a
SidHarder <softwarenavigator@gmail.com>
2017-02-27 08:53:31 -0700

    working on partials

--- build.js
+++ build.js
@@ -1,12 +1,32 @@
-var fs = require('fs')
-var stylus = require('stylus')
-var  handlebars = require('handlebars')
+const fs = require('fs')
+const stylus = require('stylus')
+const handlebars = require('handlebars')
+const readDir = require('fs-readdir-recursive')
+const path = require('path')
+const os = require('os')
 
-var data = fs.readFileSync('src/style.handlebars', 'utf8');
+var files = readDir(__dirname + '/src', function (file) {
+  var ext = path.extname(file)
+  if(ext == '') return true
+  if(ext == '.handlebars') return true
+  return false
+})
 
-var template = handlebars.precompile(data)
-handlebars.partials['styles'] = template
-console.log(handlebars.partials)
+files.forEach(function (file) {
+  var filePath =  path.join('src', file)
+  var data = fs.readFileSync(filePath, 'utf8')
+  var template = handlebars.precompile(data)
+  console.log(template)
+  handlebars.registerPartial(file, template)
+})
+
+//console.log(handlebars.partials)
+
+//var data = fs.readFileSync('src/style.handlebars', 'utf8');
+
+//var template = handlebars.precompile(data)
+//handlebars.partials['styles'] = template
+//console.log(handlebars.partials)
 
 //handlebars.templates['style'] = template
 
@@ -15,9 +35,11 @@ fs.readFile('./src/style.styl', 'utf8', function(err, str) {
     if (err) throw err
     stylus.render(str, function(err, css){
       if (err) throw err
-      fs.writeFile('./src/style.handlebars', css, function (err) {
-        console.log('all done')
-      })
+      handlebars.partials['style'] = css
+      console.log(handlebars.partials['style'])
+      //fs.writeFile('./src/style.handlebars', css, function (err) {
+      //  console.log('all done')
+      //})
     })
 })
 */
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-'use strict'
-const WomensHealthReq = require('./womens-health-req')
+var fs = require('fs')
+var Handlebars = require('handlebars/runtime');
+var templates = require('./womens-health-req/precompile.js');
 
-module.exports = class Requisition {
+module.exports = class WomensHealthReq {
   constructor () {
+    Handlebars.registerPartial('womens-health-req/page', Handlebars.templates['womens-health-req/page'])
+    Handlebars.registerPartial('womens-health-req/womens-health', Handlebars.templates['womens-health-req/womens-health'])
 
+    Handlebars.registerPartial('layouts/base', Handlebars.templates['layouts/base'])
+    Handlebars.registerPartial('partials/patient-info', Handlebars.templates['partials/patient-info'])
+    Handlebars.registerPartial('partials/billing-info', Handlebars.templates['partials/billing-info'])
+    Handlebars.registerPartial('partials/requisition-header', Handlebars.templates['partials/requisition-header'])
+    Handlebars.registerPartial('partials/tissue-biopsy', Handlebars.templates['partials/tissue-biopsy'])
   }
 
-  build(cb) {
-    const whr = new WomensHealthReq()
-    whr.build(cb)
+  build() {
+    //creates the style.css from style.style
+    //creates the precompile.js from the appropriate partials
+  }
+
+  getHTML(cb) {
+    //compiles the handlebars template and returns the html in the callback
+    var d = {
+      title: 'hello world'
+    }
+
+    var template = Handlebars.templates['pages/womens-health-req']
+    var result = template(d)
+    cb(result)
   }
 }
