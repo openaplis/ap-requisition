a2564f8e84a8f6cd9a03e92b913520595a766201
Mark Schuetze <markjschuetze@gmail.com>
2017-04-14 14:11:53 -0600

    work on reqs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 
 const path = require('path')
 const gynCytologyReq = require('./core/gyn-cytology-req.js')
+const surgicalReq = require('./core/surgical.js')
+const oncologyReq = require('./core/oncology.js')
 
 exports = module.exports = {
-    gynCytologyReq: gynCytologyReq
+    gynCytologyReq: gynCytologyReq,
+    surgicalReq: surgicalReq,
+    oncologyReq: oncologyReq
 }
--- test/requisitionTest.js
+++ test/requisitionTest.js
@@ -3,9 +3,6 @@ const path = require('path')
 const fs = require('fs')
 
 const requisitions  = require('../src/index.js')
-const requisitionPath = './requisitions/gynCytologyReq.html'
-
-console.log(requisitionPath)
 
 var data = {
   title: 'This is a test of the emergency broadcast system.'
@@ -15,6 +12,33 @@ describe('gynCytologyReq', function() {
   it('testing gynCytologyReq', function(done) {
     requisitions.gynCytologyReq.build(data, function (err, result) {
       assert.notEqual(result, null)
+      const requisitionPath = './requisitions/gynCytologyReq.html'
+      fs.writeFile(requisitionPath, result, function (err) {
+        if(err) console.log(err)
+      })
+    })
+    done()
+  })
+})
+
+describe('surgicalReq', function() {
+  it('testing surgicalReq', function(done) {
+    requisitions.surgicalReq.build(data, function (err, result) {
+      assert.notEqual(result, null)
+      const requisitionPath = './requisitions/surgicalReq.html'
+      fs.writeFile(requisitionPath, result, function (err) {
+        if(err) console.log(err)
+      })
+    })
+    done()
+  })
+})
+
+describe('oncologyReq', function() {
+  it('testing oncologyReq', function(done) {
+    requisitions.oncologyReq.build(data, function (err, result) {
+      assert.notEqual(result, null)
+      const requisitionPath = './requisitions/oncologyReq.html'
       fs.writeFile(requisitionPath, result, function (err) {
         if(err) console.log(err)
       })
