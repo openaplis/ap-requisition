68b0e622d553d8b37684072b914c212fe350ed7b
SidHarder <softwarenavigator@gmail.com>
2017-04-14 13:29:30 -0600

    saving html file

--- test/requisitionTest.js
+++ test/requisitionTest.js
@@ -1,7 +1,11 @@
 const assert = require('chai').assert
 const path = require('path')
+const fs = require('fs')
 
 const requisitions  = require('../src/index.js')
+const requisitionPath = './requisitions/gynCytologyReq.html'
+
+console.log(requisitionPath)
 
 var data = {
   title: 'This is a test of the emergency broadcast system.'
@@ -11,6 +15,9 @@ describe('gynCytologyReq', function() {
   it('testing gynCytologyReq', function(done) {
     requisitions.gynCytologyReq.build(data, function (err, result) {
       assert.notEqual(result, null)
+      fs.writeFile(requisitionPath, result, function (err) {
+        if(err) console.log(err)
+      })
     })
     done()
   })
