78d1aadfca63082e1000f4c5f30fc7b0bcd8d73f
Mark Schuetze <markjschuetze@gmail.com>
2017-04-28 16:48:02 -0600

    finishing touches to proper data input for reqs

--- src/core/ihc.js
+++ src/core/ihc.js
@@ -11,7 +11,7 @@ module.exports.build = (data, callback) => {
     Handlebars.registerPartial('ihc/page', Handlebars.templates['ihc/page'])
     Handlebars.registerPartial('style', Handlebars.templates['style'])
     Handlebars.registerPartial('layouts/base', Handlebars.templates['layouts/base'])
-    Handlebars.registerPartial('oncology/header', Handlebars.templates['oncology/header'])
+    Handlebars.registerPartial('ihc/header', Handlebars.templates['ihc/header'])
     Handlebars.registerPartial('oncology/clientandpatient-info', Handlebars.templates['oncology/clientandpatient-info'])
     Handlebars.registerPartial('ihc/ihc-menu', Handlebars.templates['ihc/ihc-menu'])
     Handlebars.registerPartial('oncology/solid-tumor-menu', Handlebars.templates['oncology/solid-tumor-menu'])
