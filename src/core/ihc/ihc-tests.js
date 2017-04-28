fe1624cfe120f515e8fa8aef0f3c7bdf891d5cc1
SidHarder <softwarenavigator@gmail.com>
2017-04-28 14:01:52 -0600

    building structure of ihc-test-data

---
+++ src/core/ihc/ihc-tests.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  groups: [
+    {
+      id: 1,
+      name: 'asdfasdf',
+      tests: [
+        { test: 'asdfasdf' },
+        { test: 'asdfasdf' }
+      ]
+    },
+    {
+      id: 2,
+      name: 'asdfasdf',
+        tests: [
+          { test: 'asdfasdf' },
+          { test: 'asdfasdf' }
+        ]
+    }
+  ]
+}
---
+++ test/ihc-tests-test.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert
+const ihcTests = require('../src/core/ihc/ihc-tests.js')
+
+describe('IHC Tests', function() {
+  describe('Test', function() {
+    it('Does it work', function(done) {
+      assert.notEqual(ihcTests, null)
+      assert.isAtLeast(ihcTests.groups.length, 1)
+      assert.isAtLeast(ihcTests.groups[0].tests.length, 1)
+      done()
+    })
+  })
+})
